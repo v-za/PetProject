78e869c94986247ed23f0a49c5430638b7a1fdf9
riekea8581 <55853139+riekea8581@users.noreply.github.com>
2019-11-03 16:36:15 -0600

    Reupload to remove description

--- the-project/application/forms.py
+++ the-project/application/forms.py
@@ -1,32 +1,18 @@
 from flask_wtf import FlaskForm
-from flask_login import current_user
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextField
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-from application.models import User
 
 #inherting FlaskForm class
 
 class UserRegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(),Length(min=2, max=40) ])
-    firstName = StringField('First Name', validators=[DataRequired()])          #other arguments are constraints
-    lastName = StringField('Last Name', validators=[DataRequired()])
+    name = StringField('Full Name', validators=[DataRequired("Too short"),Length(min=2, max=40) ])          #other arguments are constraints
     email = StringField('Email',validators=[DataRequired(),Email()])
     phone = StringField('Phone Number',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired(message="Please Enter a Password")])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
-
-    def validate_username(self,username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError("This username is taken. Please choose another one.")
-
-    def validate_email(self,email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError("This email is taken. Please choose another one.")
 
 
 class UserLoginForm(FlaskForm):
@@ -37,45 +23,13 @@ class UserLoginForm(FlaskForm):
 
 
 class AdoptionAddForm(FlaskForm):
-    name = StringField('Pet Name',validators=[DataRequired()])          #other arguments are constraints
-    type = StringField('Pet Type',validators=[DataRequired()])
-    breed = StringField('Pet Breed',validators=[DataRequired()])
-    gender = StringField('Pet Gender',validators=[DataRequired()])
-    age = IntegerField('Pet Age',validators=[DataRequired()])
-    weight = IntegerField('Pet Weight',validators=[DataRequired()])
-    #description = TextField('Pet Description', validators=[DataRequired()])
-    picture = FileField('Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    name = StringField('What is the name of the pet you are rehoming? (required)',validators=[DataRequired()])          #other arguments are constraints
+    type = StringField('What species of animal is your pet? (required)',validators=[DataRequired()])
+    breed = StringField('Do you know the breed of your pet? If so, put it here.')
+    gender = StringField('Is your pet male or female?')
+    age = IntegerField('How old is your pet? (Please put an integer. If you are unsure, you can guess)')
+    picture = FileField('Upload a pic of your pet', validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Send Request')
-
-class ProductAddForm(FlaskForm):
-    productName = StringField('Product Name',validators=[DataRequired()])          #other arguments are constraints
-    productType = StringField('Product Type',validators=[DataRequired()])
-    productDesc = TextAreaField('Product Description',validators=[DataRequired()])
-    productPrice = IntegerField('Product Price',validators=[DataRequired()])
-    productInStock = IntegerField('Product Stock',validators=[DataRequired()])
-    productImage = FileField('Product Picture', validators=[DataRequired(), FileAllowed(['jpg','jpeg','png'])])
-    submit = SubmitField('Add Product')
-
-class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(),Length(min=2, max=40) ])
-    firstName = StringField('First Name', validators=[DataRequired()])          #other arguments are constraints
-    lastName = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email',validators=[DataRequired(),Email()])
-    phone = StringField('Phone Number',validators=[DataRequired()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','jpeg','png'])])
-    submit = SubmitField('Update')
-
-    def validate_username(self,username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError("This username is taken. Please choose another one.")
-
-    def validate_email(self,email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError("This email is taken. Please choose another one.")
 
 class MeetingAddForm(FlaskForm):
     date = StringField('What date do you want to visit? Check the list below for dates already taken.', validators=[DataRequired()])
--- the-project/application/models.py
+++ the-project/application/models.py
@@ -10,16 +10,16 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20),nullable=False)
-    firstName = db.Column(db.String(40),nullable=False)
-    lastName = db.Column(db.String(40),nullable=False)
+    name = db.Column(db.String(20),nullable=False)
     email = db.Column(db.String(50),unique=True, nullable=False)
     phone = db.Column(db.String(50),unique=True, nullable=False)
     password = db.Column(db.String(50),nullable=False)
-    userPic = db.Column(db.String(20), nullable=False, default='default.png')
+
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.name}', '{self.email}', '{self.phone}')"
+
+
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,7 +29,11 @@ class Pet(db.Model):
     petBreed = db.Column(db.String(20),nullable=False)
     petAge = db.Column(db.Integer,nullable=False)
     petWeight = db.Column(db.Integer,nullable=False)
-    petImage = db.Column(db.String(20), nullable=False)
+    petImage = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+#method for how our object is printed when printeed out
+    def __repr__(self):
+        return f"Pet('{self.petName}','{self.petType}','{self.petAge}', '{self.petImage}')"
 
 class PetRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,29 +42,27 @@ class PetRequest(db.Model):
     petGender = db.Column(db.String(6))
     petBreed = db.Column(db.String(20))
     petAge = db.Column(db.Integer)
-    petContactName = db.Column(db.String(40),nullable=False)
-    petContactLast = db.Column(db.String(40),nullable=False)
+    petContactName = db.Column(db.String(80),nullable=False)
     petContactEmail = db.Column(db.String(80),nullable=False)
     petContactPhone = db.Column(db.String(80),nullable=False)
     petImage = db.Column(db.String(20), nullable=False, default='default.jpg')
-    
+
 #method for how our object is printed when printeed out
     def __repr__(self):
-        return f"Pet('{self.petName}','{self.petType}','{self.petAge}', '{self.petImage}')"
-
+        return f"PetRequest('{self.petName}','{self.petType}','{self.petContact}')"
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     productName = db.Column(db.String(40),nullable=False)
     productType = db.Column(db.String(20),nullable=False)
-    productDesc = db.Column(db.Text,nullable=False)
-    productPrice = db.Column(db.Numeric(5,2),nullable=False)
+    productDesc = db.Column(db.String(400),nullable=False)
+    productPrice = db.Column(db.Integer,nullable=False)
     productInStock = db.Column(db.Integer,nullable=False)
-    productImage = db.Column(db.String(20), nullable=False, default = 'default.jpg')
+    #productImage =
 
 #method for how our object is printed when printeed out
     def __repr__(self):
-        return f"Product('{self.productName}','{self.productType}')"
+        return f"Product('{self.productName}','{self.productType}','{self.productAge}')"
 
 class Meeting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -68,10 +70,5 @@ class Meeting(db.Model):
     meetingDate = db.Column(db.String(40),nullable=False)
     meetingEmail = db.Column(db.String(50),nullable=False)
     meetingPhone = db.Column(db.String(50),nullable=False)
-    
-#method for how our object is printed when printeed out
-    def __repr__(self):
-        return f"Meeting('{self.meetingName}','{self.meetingDate}')"
-    
-#db.drop_all()
+
 db.create_all()
